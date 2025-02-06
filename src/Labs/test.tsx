94214229ec0bd606054777cf89135c84318f65f2
Jose Annunziato <jannunzi@gmail.com>
2025-02-06 16:14:18 -0500

    added test after rebase
    test 1
    
    test 2

---
+++ src/Labs/test.tsx
@@ -0,0 +1,13 @@
+export default function Test() {
+  return (
+    <div>
+      <h1>Test</h1>
+      <p>This is a test page.</p>
+      <ul>
+        <li>Test 1</li>
+        <li>Test 2</li>
+        <li>Test 3</li>
+      </ul>
+    </div>
+  );
+}
